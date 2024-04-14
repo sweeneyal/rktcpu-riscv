386a6b516c0bd183894a8c6f58345d79bd14e99a
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2024-04-14 13:55:51 -0500

    Working github actions

--- python/run.py
+++ python/run.py
@@ -1,5 +1,13 @@
 from vunit import VUnit
-from packager import get_vhdl_files
+import pathlib
+
+def get_vhdl_files(dir, recursive=False):
+    directory = pathlib.Path(dir)
+    if recursive:
+        allVhdlFiles = list(directory.rglob('*.vhd'))
+    else:
+        allVhdlFiles = list(directory.glob('*.vhd'))
+    return allVhdlFiles
 
 # Create VUnit instance by parsing command line arguments
 vu = VUnit.from_argv(['--gtkwave-fmt', 'ghw'])
@@ -12,18 +20,18 @@ vu.add_osvvm()
 # vu.add_verilog_builtins()
 
 universal = vu.add_library("universal")
-files = get_vhdl_files('libraries/universal/hdl', recursive=True)
+files = get_vhdl_files('./libraries/universal/hdl', recursive=True)
 for file in files:
     universal.add_source_file(file)
 
 # Create library 'lib'
 scrv = vu.add_library("scrv")
-files = get_vhdl_files('hdl/rtl', recursive=True)
+files = get_vhdl_files('./hdl/rtl', recursive=True)
 for file in files:
     scrv.add_source_file(file)
 
 tb = vu.add_library("tb")
-files = get_vhdl_files('hdl/tb', recursive=True)
+files = get_vhdl_files('./hdl/tb', recursive=True)
 for file in files:
     tb.add_source_file(file)
 
